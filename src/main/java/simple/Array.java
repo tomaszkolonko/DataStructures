724185065c5aee47796f78d8b0ce4cf561f09485
tomaszkolonko <tz.kolonko@gmail.com>
2022-05-28 11:57:17 +0200

    added some bounds-checking

--- src/main/java/simple/Array.java
+++ src/main/java/simple/Array.java
@@ -11,27 +11,39 @@ public class Array {
     }
 
     public int retrieveAt(int index) {
+        checkBounds(index);
         return items[index];
     }
 
     public void insert(int item) {
-        if(pointer == items.length-1)
+        if(pointer == items.length-1) {
             expandArray();
+        }
         items[pointer] = item;
         pointer = pointer + 1;
 
     }
 
     public void removeAt(int index) {
+        checkBounds(index);
+
         items[index] = 0;
         for(int i = index; i < pointer; i++) {
             items[index] = items[index + 1];
         }
         pointer = pointer - 1;
+
+        if(pointer <= (items.length/2)) {
+            shrinkArray();
+        }
+    }
+
+    public int pointerToNextElement() {
+        return pointer;
     }
 
     public int size() {
-        return pointer;
+        return items.length;
     }
 
     public int indexOf(int item) {
@@ -47,8 +59,22 @@ public class Array {
 
     }
 
+    private void checkBounds(final int index) {
+        if(index < 0 || index > pointer) {
+            throw new IllegalArgumentException("no item at this index");
+        }
+    }
+
     private void expandArray() {
-        int[] newArray = new int[pointer*2];
+        int[] newArray = new int[items.length * 2];
+        for(int i = 0; i <= pointer; i++) {
+            newArray[i] = items[i];
+        }
+        items = newArray;
+    }
+
+    private void shrinkArray() {
+        int[] newArray = new int[items.length / 2];
         for(int i = 0; i <= pointer; i++) {
             newArray[i] = items[i];
         }
--- src/test/java/simple/ArrayTest.java
+++ src/test/java/simple/ArrayTest.java
@@ -22,18 +22,42 @@ class ArrayTest {
     void retrieveAt() {
         assertEquals(11, items.retrieveAt(0));
         assertEquals(14, items.retrieveAt(3));
+
+        assertThrows(IllegalArgumentException.class, () -> items.retrieveAt(10));
+        assertThrows(IllegalArgumentException.class, () -> items.retrieveAt(-1));
     }
 
     @Test
     void insertIntoExpandedArray() {
         items.insert(15);
+
+        assertEquals(5, items.pointerToNextElement());
+        assertEquals(11, items.retrieveAt(0));
+        assertEquals(12, items.retrieveAt(1));
+        assertEquals(13, items.retrieveAt(2));
+        assertEquals(14, items.retrieveAt(3));
         assertEquals(15, items.retrieveAt(4));
     }
 
     @Test
     void removeAt() {
         items.removeAt(3);
-        assertEquals(3, items.size());
+        assertEquals(3, items.pointerToNextElement());
+
+        assertThrows(IllegalArgumentException.class, () -> items.removeAt(10));
+        assertThrows(IllegalArgumentException.class, () -> items.removeAt(-1));
+    }
+
+    @Test
+    void removeAtWithShrinkingArray() {
+        assertEquals(8, items.size());
+        items.removeAt(3);
+        assertEquals(4, items.size());
+
+        assertEquals(3, items.pointerToNextElement());
+        assertEquals(11, items.retrieveAt(0));
+        assertEquals(12, items.retrieveAt(1));
+        assertEquals(13, items.retrieveAt(2));
     }
 
     @Test
@@ -41,5 +65,6 @@ class ArrayTest {
         assertEquals(0, items.indexOf(11));
         assertEquals(2, items.indexOf(13));
 
+        assertEquals(-1, items.indexOf(100));
     }
 }
